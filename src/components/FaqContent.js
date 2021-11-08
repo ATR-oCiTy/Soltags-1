4de13fc7eeeb1b86cfc24c74bc111f3593f4988b
KiranJ2000 <kiranjanardhanan2000@gmail.com>
2021-11-08 07:28:54 +0530

    added gradient to faq border

--- src/components/FaqContent.js
+++ src/components/FaqContent.js
@@ -10,7 +10,16 @@ import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 const FaqComp = (props) => {
   return (
     <Grid item xs={12}>
-      <Box border={3} borderRadius='borderRadius' mx={5} my={1} className="faqBorder">
+      <Box
+        border={3}
+        borderRadius='borderRadius'
+        mx={5}
+        my={1}
+        style={{
+          borderImageSlice: "1",
+          borderImageSource: "linear-gradient(#DC1FFF, #03E1FF, #00FFA3)",
+        }}
+      >
         <Accordion square={true}>
           <AccordionSummary expandIcon={<ExpandMoreIcon />}>
             <Box display='flex' flexDirection='row' color='white'>
